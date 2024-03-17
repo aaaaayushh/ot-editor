acf3977ab3f0c09c54a88599cbb758d06c120776
AayushShah <aayushshah1142@gmail.com>
2024-03-17 15:49:08 +0530

    added comments to ot operations, basic cli setup for client

--- client/main.go
+++ client/main.go
@@ -1,80 +1,149 @@
 package main
 
-// TODO go install github.com/dkorunic/betteralign/cmd/betteralign@latest
 import (
-	"bufio"
 	"fmt"
-	"github.com/gorilla/websocket"
-	"log"
-	"net/http"
+	"github.com/charmbracelet/bubbles/textarea"
+	tea "github.com/charmbracelet/bubbletea"
 	"os"
-	"strings"
 )
 
-func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("Usage: go run client.go <client_id>")
-		return
-	}
-	serverAddr := "ws://localhost:8080/ws"
+type model struct {
+	err      error
+	textArea textarea.Model
+}
+type errMsg error
 
-	req, err := http.NewRequest("GET", serverAddr, nil)
-	if err != nil {
-		log.Fatal("Error creating web socket request:", err)
-	}
-
-	clientId := os.Args[1]
-	req.Header.Set("clientId", clientId)
-	conn, _, err := websocket.DefaultDialer.Dial(serverAddr, req.Header)
-	if err != nil {
-		log.Fatal("Error establishing websocket connection:", err)
-	}
-	defer conn.Close()
-
-	go readMessages(conn)
-	sendMessages(conn)
+// Init can return a Cmd that could perform some initial I/O
+func (m model) Init() tea.Cmd {
+	return nil
 }
 
-func readMessages(conn *websocket.Conn) {
-	for {
-		_, message, err := conn.ReadMessage()
-		if err != nil {
-			log.Println("Error reading message:", err)
-			return
-		}
-		fmt.Printf("\nReceived: %s\n", message)
+type msgMoveCursor int
+type msgInsertChar rune
+
+// Initialize the application state
+func initialModel() model {
+	ti := textarea.New()
+	ti.Placeholder = "Type something.."
+	ti.Focus()
+
+	return model{
+		textArea: ti,
+		err:      nil,
 	}
+
 }
 
-func sendMessages(conn *websocket.Conn) {
-	reader := bufio.NewReader(os.Stdin)
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmds []tea.Cmd
+	var cmd tea.Cmd
 
-	for {
-		fmt.Print("> ")
-		message, err := reader.ReadString('\n')
-		if err != nil {
-			log.Println("Error reading message:", err)
-			return
-		}
-
-		// Remove the newline character from the input
-		message = strings.TrimSuffix(message, "\n")
-
-		// Check if the user wants to disconnect
-		if message == "disconnect" {
-			err = conn.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
-			if err != nil {
-				log.Println("Error sending disconnect message:", err)
-				return
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.Type {
+		case tea.KeyEsc:
+			if m.textArea.Focused() {
+				m.textArea.Blur()
 			}
-			log.Println("Disconnected from server")
-			return
+		case tea.KeyCtrlC:
+			return m, tea.Quit
+		default:
+			if !m.textArea.Focused() {
+				cmd = m.textArea.Focus()
+				cmds = append(cmds, cmd)
+			}
 		}
+	case errMsg:
+		m.err = msg
+		return m, nil
+	}
 
-		err = conn.WriteMessage(websocket.TextMessage, []byte(message))
-		if err != nil {
-			log.Println("Error sending message:", err)
-			return
-		}
+	m.textArea, cmd = m.textArea.Update(msg)
+	cmds = append(cmds, cmd)
+
+	return m, tea.Batch(cmds...)
+}
+
+// View function to render the UI
+func (m model) View() string {
+	return fmt.Sprintf("Shuru kare?\n\n\n\n%s", m.textArea.View())
+}
+
+func main() {
+	// Initialize the Bubble Tea program with the initial model, update function, and view function
+	p := tea.NewProgram(initialModel())
+
+	// Run the program
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Error running program: %v\n", err)
+		os.Exit(1)
 	}
 }
+
+//func main() {
+//	if len(os.Args) < 2 {
+//		fmt.Println("Usage: go run client.go <client_id>")
+//		return
+//	}
+//	serverAddr := "ws://localhost:8080/ws"
+//
+//	req, err := http.NewRequest("GET", serverAddr, nil)
+//	if err != nil {
+//		log.Fatal("Error creating web socket request:", err)
+//	}
+//
+//	clientId := os.Args[1]
+//	req.Header.Set("clientId", clientId)
+//	conn, _, err := websocket.DefaultDialer.Dial(serverAddr, req.Header)
+//	if err != nil {
+//		log.Fatal("Error establishing websocket connection:", err)
+//	}
+//	defer conn.Close()
+//
+//	go readMessages(conn)
+//	sendMessages(conn)
+//}
+//
+//func readMessages(conn *websocket.Conn) {
+//	for {
+//		_, message, err := conn.ReadMessage()
+//		if err != nil {
+//			log.Println("Error reading message:", err)
+//			return
+//		}
+//		fmt.Printf("\nReceived: %s\n", message)
+//	}
+//}
+//
+//func sendMessages(conn *websocket.Conn) {
+//	reader := bufio.NewReader(os.Stdin)
+//
+//	for {
+//		fmt.Print("> ")
+//		message, err := reader.ReadString('\n')
+//		if err != nil {
+//			log.Println("Error reading message:", err)
+//			return
+//		}
+//
+//		// Remove the newline character from the input
+//		message = strings.TrimSuffix(message, "\n")
+//
+//		// Check if the user wants to disconnect
+//		if message == "disconnect" {
+//			err = conn.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+//			if err != nil {
+//				log.Println("Error sending disconnect message:", err)
+//				return
+//			}
+//			log.Println("Disconnected from server")
+//			return
+//		}
+//
+//		err = conn.WriteMessage(websocket.TextMessage, []byte(message))
+//		if err != nil {
+//			log.Println("Error sending message:", err)
+//			return
+//		}
+//	}
+//}
--- server/ot/algo.go
+++ server/ot/algo.go
@@ -15,24 +15,36 @@ func TransformOperation(op *operation.Operation, c *client.Client) *operation.Op
 }
 
 func TransformPair(op1, op2 *operation.Operation) *operation.Operation {
+	// if both operations are from the same client, and op1 was generated before op2, return op1
+	// because op1 happened before op2, hence not transformations are needed
 	if op1.ID.ClientID == op2.ID.ClientID && op1.ID.SequenceNum < op2.ID.SequenceNum {
 		return op1
 	}
 
+	// else transform the operations according to op2's type
 	if op2.Type == operation.Insert {
 		return TransformInsert(op1, op2)
 	}
 	return TransformDelete(op1, op2)
 }
 
+// TransformInsert transforms op1 according to op2, which is supposed to be an INSERTION
 func TransformInsert(op1, op2 *operation.Operation) *operation.Operation {
 	if op1.Type == operation.Insert {
+		/* if op1 is also an insertion
+		1. if op1's position is less than op2's position, no transformation is needed OR
+		2. The rule used here is that if two insert operations have the same position, the operation from the client
+			with the smaller ClientID is considered to have occurred first. This is an arbitrary rule, but it ensures
+			consistency across all clients.
+		*/
 		if op1.Position < op2.Position || (op1.Position == op2.Position && op1.ID.ClientID < op2.ID.ClientID) {
 			return op1
 		}
+		// otherwise shift the position of the insert operation by op2's content length
 		op1.Position += len(op2.Content)
 		return op1
 	}
+	// if op1 is a deletion, and op1's position is less than or equal to op2's position, no transformation is needed
 	if op1.Position <= op2.Position {
 		return op1
 	}
@@ -40,17 +52,29 @@ func TransformInsert(op1, op2 *operation.Operation) *operation.Operation {
 	return op1
 }
 
+// TransformDelete transforms op1 according to op2, which is supposed to be a DELETION
 func TransformDelete(op1, op2 *operation.Operation) *operation.Operation {
 	if op1.Type == operation.Delete {
+		/* if op1 is also a deletion
+		`	1. if op1's position is less than op2's position, no transformation is needed OR
+			2. The rule used here is that if two delete operations have the same position, the operation from the client
+				with the smaller ClientID is considered to have occurred first. This is an arbitrary rule, but it ensures
+				consistency across all clients.
+		*/
 		if op1.Position < op2.Position || (op1.Position == op2.Position && op1.ID.ClientID < op2.ID.ClientID) {
 			return op1
 		}
+
+		// otherwise shift the position of the delete operation by op2's content length
 		op1.Position -= len(op2.Content)
 		return op1
 	}
+	// if op1 is an insertion, and op1's position is less than or equal to op2's position, no transformation is needed
 	if op1.Position <= op2.Position {
 		return op1
 	}
+	// if op1's position is less than op2's position + op2's content length, then op1's content is split into two parts
+	// and the part that overlaps with op2's content is removed
 	if op1.Position < op2.Position+len(op2.Content) {
 		op1.Content = op1.Content[:op2.Position-op1.Position] + op1.Content[op2.Position-op1.Position+len(op2.Content):]
 		op1.Position = op2.Position
